7cbb7764cf6e9d52c0c0386ff69396c7ec8022d2
CritickalGames <zgtale1@hotmail.com>
2022-02-23 10:46:40 -0300

    .

--- Index.php
+++ Index.php
@@ -1,1 +1,26 @@
-<?php?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!--<script  src="reCodigo.js"></script>-->
+
+	<link rel="shortcut icon" href="Reutilizar/Icono.ico"><!--ICONO FALTANTE EN TODAS LAS PAGINAS-->
+	<link rel="stylesheet" type="text/css" href="css.css">
+
+	<meta charset="utf-8">
+	<meta name="Indice del juego" content="Portale a todos las reglas">
+	<title>DICE GAME index</title>
+</head>
+<body onload="color();">
+    <center><h1>Reglas del juego</h1></center>
+    <center><h3>DICE GAME</h3></center>
+		
+	<?php include ('Reusar/navIndice.php');?><!--CREAR NAV DE TIENDAS-->
+	
+	<div class="forma" id="indice">
+		<h2 align="center">Indice</h2>	
+	</div>
+
+	<!--Contenido-->
+
+</body>
+</html>
