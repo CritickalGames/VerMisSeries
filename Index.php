97d491f0f7711c5bcf12324c5e87a6a6c3f799c0
CritickalGames <zgtale1@hotmail.com>
2022-02-23 18:27:27 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -8,13 +8,11 @@
 
 	<meta charset="utf-8">
 	<meta name="Indice del juego" content="Portale a todos las reglas">
-	<title>DICE GAME index</title>
+	<title></title>
 </head>
 <body onload="color();">
-    <center><h1>Reglas del juego</h1></center>
-    <center><h3>DICE GAME</h3></center>
-		
-	<?php include ('Reusar/navIndice.php');?><!--CREAR NAV DE TIENDAS-->
+
+	<!--CREAR NAV DE TIENDAS-->
 	
 	<div class="forma" id="indice">
 		<h2 align="center">Indice</h2>	
