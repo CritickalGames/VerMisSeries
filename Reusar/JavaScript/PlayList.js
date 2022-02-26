4f193e175dbda46f13f25b4402d7171e91ecbf58
CritickalGames <zgtale1@hotmail.com>
2022-02-26 19:43:37 -0300

    diccionario "lista" agregado
    También "newValue" pasó a ser "cap"

--- Reusar/JavaScript/PlayList.js
+++ Reusar/JavaScript/PlayList.js
@@ -1,19 +1,23 @@
 init();
 
+var lista={
+  1:"1.mp4",
+  2:"2.mp4"
 
+};
 
 function next(){
   var reproducor = 
     document.getElementsByTagName("video");
   var srcReproductor = reproducor[0].getAttribute("src");
-  var newValue = "2.mp4";
+  var cap = "2.mp4";
   var i = srcReproductor.indexOf("video");
   
 
   if(i>-1){
     i = (i+5);
     var changestr = srcReproductor.substring(i,srcReproductor.length);
-    var nuevoVideo = srcReproductor.substring(0,i) + newValue;
+    var nuevoVideo = srcReproductor.substring(0,i) + cap;
     reproducor[0].setAttribute("src", nuevoVideo);
   }
 }
@@ -22,14 +26,14 @@ function back(){
   var reproducor = 
     document.getElementsByTagName("video");
   var srcReproductor = reproducor[0].getAttribute("src");
-  var newValue = "1.mp4";
+  var cap = "1.mp4";
   var i = srcReproductor.indexOf("video");
   
 
   if(i>-1){
     i = (i+5);
     var changestr = srcReproductor.substring(i,srcReproductor.length);
-    var nuevoVideo = srcReproductor.substring(0,i) + newValue;
+    var nuevoVideo = srcReproductor.substring(0,i) + cap;
     reproducor[0].setAttribute("src", nuevoVideo);
   }
 }
