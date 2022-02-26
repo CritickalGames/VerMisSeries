13fc2b5db34f732b87744e3ec4bb985e84946e73
CritickalGames <zgtale1@hotmail.com>
2022-02-26 20:30:19 -0300

    Principio de Función única
    setReproductor
    setSrcReproductor
    setIndice

--- Reusar/JavaScript/PlayList.js
+++ Reusar/JavaScript/PlayList.js
@@ -13,17 +13,28 @@ function setReproductor(){
     return reproducor;
 }
 
-function next(){
+function setSrcReproductor(){
   reproducor= setReproductor();
   var srcReproductor = reproducor[0].getAttribute("src");
-  var cap = "2.mp4";
-  var i = srcReproductor.indexOf("video");
-  
+  return srcReproductor;
+}
 
-  if(i>-1){
-    i = (i+5);
-    var changestr = srcReproductor.substring(i,srcReproductor.length);
-    var nuevoVideo = srcReproductor.substring(0,i) + cap;
+
+function setIndice(){
+  setReproductor=setSrcReproductor();
+  var indice = srcReproductor.indexOf("video");
+  return indice;
+}
+
+function next(){
+  var cap = "2.mp4";
+  srcReproductor=setSrcReproductor();
+  indice=setIndice();
+
+  if(indice>-1){
+    indice = (indice+5);
+    var changestr = srcReproductor.substring(indice,srcReproductor.length);
+    var nuevoVideo = srcReproductor.substring(0,indice) + cap;
     reproducor[0].setAttribute("src", nuevoVideo);
   }
 }
