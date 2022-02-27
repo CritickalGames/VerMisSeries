ba06dd22a32798aa024f2ec9bbd40040dc9b3c88
CritickalGames <zgtale1@hotmail.com>
2022-02-26 23:04:26 -0300

    Revert "diccionario "lista" agregado"
    This reverts commit 4f193e175dbda46f13f25b4402d7171e91ecbf58.

--- Reusar/JavaScript/PlayList.js
+++ Reusar/JavaScript/PlayList.js
@@ -1,10 +1,6 @@
 init();
 
-var lista={
-  1:"1.mp4",
-  2:"2.mp4"
 
-};
 
 
 <<<<<<< HEAD
@@ -18,14 +14,14 @@ function setReproductor(){
 function next(){
   reproducor= setReproductor();
   var srcReproductor = reproducor[0].getAttribute("src");
-  var cap = "2.mp4";
+  var newValue = "2.mp4";
   var i = srcReproductor.indexOf("video");
   
 
   if(i>-1){
     i = (i+5);
     var changestr = srcReproductor.substring(i,srcReproductor.length);
-    var nuevoVideo = srcReproductor.substring(0,i) + cap;
+    var nuevoVideo = srcReproductor.substring(0,i) + newValue;
     reproducor[0].setAttribute("src", nuevoVideo);
   }
 }
@@ -35,14 +31,14 @@ function back(){
   var reproducor = 
     document.getElementsByTagName("video");
   var srcReproductor = reproducor[0].getAttribute("src");
-  var cap = "1.mp4";
+  var newValue = "1.mp4";
   var i = srcReproductor.indexOf("video");
   
 
   if(i>-1){
     i = (i+5);
     var changestr = srcReproductor.substring(i,srcReproductor.length);
-    var nuevoVideo = srcReproductor.substring(0,i) + cap;
+    var nuevoVideo = srcReproductor.substring(0,i) + newValue;
     reproducor[0].setAttribute("src", nuevoVideo);
   }
 }**/
