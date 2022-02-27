f7867fba10379f5904f66ba79df369f4725bd4d2
CritickalGames <zgtale1@hotmail.com>
2022-02-26 23:03:14 -0300

    sep

--- Reusar/JavaScript/PlayList.js
+++ Reusar/JavaScript/PlayList.js
@@ -7,6 +7,29 @@ var lista={
 };
 
 
+<<<<<<< HEAD
+=======
+function setReproductor(){
+    var reproducor = 
+    document.getElementsByTagName("video");
+    return reproducor;
+}
+
+function next(){
+  reproducor= setReproductor();
+  var srcReproductor = reproducor[0].getAttribute("src");
+  var cap = "2.mp4";
+  var i = srcReproductor.indexOf("video");
+  
+
+  if(i>-1){
+    i = (i+5);
+    var changestr = srcReproductor.substring(i,srcReproductor.length);
+    var nuevoVideo = srcReproductor.substring(0,i) + cap;
+    reproducor[0].setAttribute("src", nuevoVideo);
+  }
+}
+>>>>>>> parent of 13fc2b5 (Principio de Función única)
 /**
 function back(){
   var reproducor = 
